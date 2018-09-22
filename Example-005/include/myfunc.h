7c5ad3ef5fa82d2feca50ee25fe099d2ba157cc9
David Canino <canino.david@gmail.com>
2018-09-22 09:47:31 +0200

    Finalized the 'Example-005' test

---
+++ Example-005/include/myfunc.h
@@ -0,0 +1,19 @@
+/*************************************************************************************************************************
+ * Examples in the C++ language (the 'Example-005' Test).
+ *
+ * GitHub repository: https://github.com/davidcanino/C-plus-plus-Examples
+ *
+ * Created by David Canino (canino.david@gmail.com), September 2018.
+ *
+ * myfunc.h - the header file, where the 'myfunc()' function, used by the 'Example-005' Test, is defined.
+ *************************************************************************************************************************/
+
+/* Optional inclusion! */
+#ifndef MY_FUNC_H
+
+	#define MY_FUNC_H
+
+	/// This function does nothing basically.	
+	void myfunc(void);
+
+#endif
---
+++ Example-005/src/myfunc.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************************************************************
+ * Examples in the C++ language (the 'Example-005' Test).
+ *
+ * GitHub repository: https://github.com/davidcanino/C-plus-plus-Examples
+ *
+ * Created by David Canino (canino.david@gmail.com), September 2018.
+ *
+ * myfunc.cpp - the source file, where the 'myfunc()' function, used by the 'Example-005' Test, is implemented.
+ *************************************************************************************************************************/
+
+#include "myfunc.h"
+#include <iostream>
+using namespace std;
+
+void myfunc(void)
+{
+	cout << "\tThis is the 'myfunc()' function in the 'Example-005' Test." << endl << endl;
+	cout.flush();
+}
