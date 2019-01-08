33f3df7a595f50e39689e38941e78380fef1c895
David Canino <canino.david@gmail.com>
2019-01-08 23:26:15 +0100

    Added the CMake infrastructure for the 'Example-002' test

---
+++ Example-002/src/main.cpp
@@ -0,0 +1,32 @@
+/**********************************************************************************************
+ * Examples in the C++ language (the 'Example-002' Test).
+ *
+ * GitHub repository: https://github.com/davidcanino/C-plus-plus-Examples
+ *
+ * Created by David Canino (canino.david@gmail.com), January 2019.
+ *
+ * main.cpp - the C++ source file, implementing the main function for the 'Example-002' Test.
+ **********************************************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+/// The main function for the <i>'Example-002'</i> Test.
+int main(void)
+{
+	/* This is the 'Example-002' Test, near to be the classic 'Hello World!' example! */
+	puts("\n\tThis is the \'Example-002\' Test in the C++ language.");
+	puts("\t=====================================================\n");
+	puts("\tHello World!\n");
+	puts("\t=====================================================\n");
+	fprintf(stdout,"\tPress the <RETURN> key to finish ... ");
+	fflush(stdout);
+	getchar();
+	#ifndef _MSC_VER
+		puts("");
+		fflush(stdout);
+	#endif
+	return EXIT_SUCCESS;
+}
+
